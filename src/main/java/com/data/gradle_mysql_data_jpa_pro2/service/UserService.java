3cb2ef5cb7fc46704ba2e38812e28a9dca91c69e
Aryan Shinde <aryanshinde3002@gmail.com>
2025-07-07 18:47:31 +0530

    UserService.java updated

--- src/main/java/com/data/gradle_mysql_data_jpa_pro2/service/UserService.java
+++ src/main/java/com/data/gradle_mysql_data_jpa_pro2/service/UserService.java
@@ -6,16 +6,16 @@ import com.data.gradle_mysql_data_jpa_pro2.dto.UserDto;
 
 public interface UserService {
 
-    public UserDto saveUser(UserDto userDto);
+    UserDto saveUser(UserDto userDto);
 
-    public List<UserDto> fetchUserList();
+    List<UserDto> fetchUserList();
 
-    public UserDto updateUser(UserDto userDto);
+    UserDto updateUser(UserDto userDto);
 
-    public void deleteUser(Integer id);
+    void deleteUser(Integer id);
 
-    public UserDto updatePartialUser(Integer id,UserDto partialUser);
+    UserDto updatePartialUser(Integer id,UserDto partialUser);
 
-    public UserDto getUserById(Integer id);
-    
+    UserDto getUserById(Integer id);
+
 }
