27ce21c49d451bf918e1efc3fd529c78fd6e4fd0
Aryan Shinde <aryanshinde3002@gmail.com>
2025-07-07 22:14:05 +0530

    errors solved @Autowired error

--- src/main/java/com/data/gradle_mysql_data_jpa_pro2/GradleMysqlDataJpaPro2Application.java
+++ src/main/java/com/data/gradle_mysql_data_jpa_pro2/GradleMysqlDataJpaPro2Application.java
@@ -1,12 +1,18 @@
 package com.data.gradle_mysql_data_jpa_pro2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.data.gradle_mysql_data_jpa_pro2.controller.UserController;
+
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class GradleMysqlDataJpaPro2Application {
 
+	@Autowired
+	private UserController userController;
 
 	public static void main(String[] args) {
 
@@ -15,7 +21,7 @@ public class GradleMysqlDataJpaPro2Application {
 
 	@PostConstruct
 	public void init() {
-		
+		userController.getUser();
 	}
 
 }
